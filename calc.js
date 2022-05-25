fa9857ea4c02cdd990349f356497f0d1d51f1019
Sean Ryerson <seanclancyryerson@gmail.com>
2022-05-25 17:33:32 -0400

    Create and test add, subtract, multiply, and divide functions

---
+++ calc.js
@@ -0,0 +1,22 @@
+
+console.log(add(5,6));
+console.log(subtract(5,6));
+console.log(multiply(5,6));
+console.log(divide(5,6));
+
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
