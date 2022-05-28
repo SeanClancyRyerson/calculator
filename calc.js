e22be0f061fcdc77bd79afd369b9076ed508d4e0
Sean Ryerson <seanclancyryerson@gmail.com>
2022-05-28 15:05:16 -0400

    Create test function for buttons

--- calc.js
+++ calc.js
@@ -1,43 +1,45 @@
-
-operate('+',5,6);
-operate('-',5,6);
-operate('*',5,6);
-operate('/',5,6);
-
+operate("+", 5, 6);
+operate("-", 5, 6);
+operate("*", 5, 6);
+operate("/", 5, 6);
 
 function add(x, y) {
-    return x + y;
+  return x + y;
 }
 
 function subtract(x, y) {
-    return x - y;
+  return x - y;
 }
 
 function multiply(x, y) {
-    return x * y;
+  return x * y;
 }
 
 function divide(x, y) {
-    return x / y;
+  return x / y;
 }
 
 /*Create a new function operate that takes an operator and 2 numbers and then 
 calls one of the above functions on the numbers. */
 function operate(op, x, y) {
-    let ans;
-    switch(op) {
-        case '+':
-            ans = add(x, y);
-            break;
-        case '-':
-            ans = subtract(x, y);
-            break;
-        case '*':
-            ans = multiply(x, y);
-            break;
-        case '/':
-            ans = divide(x, y);
-            break;
-    }
-    console.log(ans);
+  let ans;
+  switch (op) {
+    case "+":
+      ans = add(x, y);
+      break;
+    case "-":
+      ans = subtract(x, y);
+      break;
+    case "*":
+      ans = multiply(x, y);
+      break;
+    case "/":
+      ans = divide(x, y);
+      break;
+  }
+  console.log(ans);
+}
+
+function buttonPress(e) {
+  console.log(e);
 }
